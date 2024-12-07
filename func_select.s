9799169f85aaa65714efe0a6b6b4d4a91a043706
SorerBOT <alonfiller@gmail.com>
2024-12-07 19:46:51 +0200

    (Feat) ziBLACKing works

--- func_select.s
+++ func_select.s
@@ -1,8 +1,11 @@
 .section    .rodata
     quad_fmt:
 	    .string "%ld\n"
+    pstrlen_fmt:
+	    .string "first pstring length: %d, second pstring length: %d\n"
 
 .section    .text
+.extern     pstrlen
 .globl  run_func
 .type   run_func, @function
     run_func:
@@ -11,20 +14,30 @@
 	    movq	%rsp, %rbp
 
         cmpq    $31, %rdi
-        je
+        je      option_31
 
-        strlen:
-            jmp    epilogue
-        swapCase:
-            jmp    epilogue
-        strcpy:
-            jmp    epilogue
-        strcat:
-            jmp    epilogue
+        option_31:
+            movq    %rsi, %rdi
+            call    pstrlen
 
+            movq    %rax, %rsi
+            movq    %rdx, %rdi
+            call    pstrlen
+
+            movq    $pstrlen_fmt, %rdi
+            movq    %rax, %rdx
+            call    printf
+
+            jmp    epilogue
+        option_33:
+            jmp    epilogue
+        option_34:
+            jmp    epilogue
+        option_37:
+            jmp    epilogue
 
         # epilogue
         epilogue:
-	        movq	%rbp, %rsp
-	        popq	%rbp
-	        ret
+            movq     $60, %rax
+            xorq     %rdi, %rdi
+            syscall
--- pstring.s
+++ pstring.s
@@ -1,14 +1,58 @@
 .section    .text
-.globl      strlen
-.type       strlen, @function
-    strlen:
-        # prologue
-	    pushq	%rbp
-	    movq	%rsp, %rbp
+.global      pstrlen
+.type       pstrlen, @function
+.globl      swapCase
+.type       swapCase, @function
+.globl      strcpy
+.type       strcpy, @function
+.globl      strcat
+.type       strcat, @function
+
+pstrlen:
+    # prologue
+	pushq	%rbp
+	movq	%rsp, %rbp
+
+    movzbq  (%rdi), %rax
+
+    #epilogue
+	movq	%rbp, %rsp
+	popq	%rbp
+	ret
+
+
+swapCase:
+    # prologue
+	pushq	%rbp
+	movq	%rsp, %rbp
 
 
 
-        #epilogue
-	    movq	%rbp, %rsp
-	    popq	%rbp
-	    ret
+    #epilogue
+	movq	%rbp, %rsp
+	popq	%rbp
+	ret
+
+strcpy:
+    # prologue
+	pushq	%rbp
+	movq	%rsp, %rbp
+
+
+
+    #epilogue
+	movq	%rbp, %rsp
+	popq	%rbp
+	ret
+
+strcat:
+    # prologue
+	pushq	%rbp
+	movq	%rsp, %rbp
+
+
+
+    #epilogue
+	movq	%rbp, %rsp
+	popq	%rbp
+	ret
