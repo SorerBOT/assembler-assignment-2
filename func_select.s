671cb6fef839be3c0781f5c444075a102e30a617
SorerBOT <alonfiller@gmail.com>
2024-12-08 20:55:04 +0200

    (Patch) now printing pstrings correctly

--- func_select.s
+++ func_select.s
@@ -5,9 +5,16 @@
 	    .string "first pstring length: %d, second pstring length: %d\n"
     swapCase_fmt:
         .string "length: %d, string: %s\n"
+    int_fmt:
+        .string " %d"
+    unsigned_char_fmt:
+        .string " %hhu"
 .section    .text
+.extern     printf
+.extern     scanf
 .extern     pstrlen
 .extern     swapCase
+.extern     pstrijcpy
 .globl  run_func
 .type   run_func, @function
     run_func:
@@ -19,6 +26,8 @@
         je      option_31
         cmpq    $33, %rdi
         je      option_33
+        cmpq    $34, %rdi
+        je      option_34
         jmp     epilogue
         option_31:
             movq    %rsi, %rdi
@@ -34,12 +43,12 @@
 
             jmp    epilogue
         option_33:
-            movq    %rsi, %r12
-            movq    %rdx, %r13
+            movq    %rsi, %r12 # destination [len, char[255]]
+            movq    %rdx, %r13 # src
             # performing swapCase on the first string
             movq    %r12, %rdi
             call    swapCase
-            movq    %rdi, %rdx
+            leaq    1(%rdi), %rdx
             call    pstrlen
             movq    %rax, %rsi
             movq    $swapCase_fmt, %rdi
@@ -47,13 +56,61 @@
             # performing swapCase on the second string
             movq    %r13, %rdi
             call    swapCase
-            movq    %rdi, %rdx
+            leaq    1(%rdi), %rdx
             call    pstrlen
             movq    %rax, %rsi
             movq    $swapCase_fmt, %rdi
             call    printf
             jmp     epilogue
         option_34:
+            movq    %rsi, %r12
+            movq    %rdx, %r13
+            # scanning for numbers (i,j)
+            subq    $16, %rsp
+            movq    $int_fmt, %rdi
+            movq    %rsp, %rsi
+            call    scanf # i
+
+            movq    $int_fmt, %rdi
+            leaq    8(%rsp), %rsi
+            call    scanf # j
+
+            # re-arranging registers
+            movzbq    8(%rsp), %rcx
+            movzbq    (%rsp), %rdx
+            movq    %r12, %rdi
+            movq    %r13, %rsi
+
+            # restoring stack
+            addq    $16, %rsp
+            # resetting registers
+            xorq    %r10, %r10
+            xorq    %r11, %r11
+            xorq    %r12, %r12
+            xorq    %r13, %r13
+            # calling function
+            call    pstrijcpy
+            # moving to temp
+            movq    %rax, %r13 # destination
+            movq    %rsi, %r12 # src
+            # printing destination
+            movq    %r13, %rdi
+            call    pstrlen
+            movq    %rax, %rsi
+            # xor-ing rax
+            xorq    %rax, %rax
+            leaq    1(%r13), %rdx
+            movq    $swapCase_fmt, %rdi
+            call    printf
+            # printing src
+            movq    %r12, %rdi
+            call    pstrlen
+            movq    %rax, %rsi
+            leaq    1(%r12), %rdx
+            movq    $swapCase_fmt, %rdi
+            # xor-ing rax
+            xorq    %rax, %rax
+            call    printf
             jmp    epilogue
         option_37:
             jmp    epilogue
--- pstring.s
+++ pstring.s
@@ -1,10 +1,14 @@
+.section .rodata
+    invalidInput_fmt:
+        .string "invalid input!\n"
+
 .section    .text
 .global      pstrlen
 .type       pstrlen, @function
 .globl      swapCase
 .type       swapCase, @function
-.globl      strcpy
-.type       strcpy, @function
+.globl      pstrijcpy
+.type       pstrijcpy, @function
 .globl      strcat
 .type       strcat, @function
 
@@ -64,17 +68,63 @@ swapCase:
 	    popq	%rbp
 	    ret
 
-strcpy:
+pstrijcpy:
     # prologue
 	pushq	%rbp
 	movq	%rsp, %rbp
 
+    # temps
+    movq    %rdi, %r12 # destination
+    movq    %rsi, %r13 # src
+    xorq    %r8, %r8
 
+    # validating input
+    # if j < i
+    cmpq    %rdx, %rcx
+    jl      invalidInput
+    # if i < 0
+    cmpq    $0, %rdx
+    jl      invalidInput
+    # checking for the length of destination
+    movq    %r12, %rdi
+    call    pstrlen
+    movq    %rax, %r11
+    # checking for the length of src
+    movq    %r13, %rdi
+    call    pstrlen
+    movq    %rax, %r10
+    # if j >= src.length
+    cmpq    %rcx, %r10
+    jle     invalidInput
+    # if j >= destination.length
+    cmpq    %rcx, %r11
+    jle     invalidInput
+    jmp     cpy_iteration
 
-    #epilogue
-	movq	%rbp, %rsp
-	popq	%rbp
-	ret
+    invalidInput:
+        movq    $invalidInput_fmt, %rdi
+        call    printf
+        jmp     finishedCpy
+
+    # the cpy function
+    cpy_iteration:
+        # if j < i
+        cmpq    %rdx, %rcx
+        jl      finishedCpy
+        # copying the i'th char from src
+        movzbq  1(%r13, %rdx), %r8
+        # moving it to destination
+        movb    %r8b, 1(%r12, %rdx)
+        # increment i
+        incq     %rdx
+        jmp     cpy_iteration
+
+    finishedCpy:
+        #epilogue
+        movq    %r12, %rax # returning destination
+	    movq	%rbp, %rsp
+	    popq	%rbp
+	    ret
 
 strcat:
     # prologue
